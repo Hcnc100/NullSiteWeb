30462f3748faef6a46812c5500d1310162efdcb3
Hcnc100 <ricardo.pajaro.bc2d@gmail.com>
2023-12-27 15:31:41 -0600

    Refactor app component and resize service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {ResizeService} from "./services/resize/resize.service";
+import { Component, OnInit } from '@angular/core';
+import { ResizeService } from "./services/resize/resize.service";
 
 @Component({
   selector: 'app-root',
@@ -8,16 +8,10 @@ import {ResizeService} from "./services/resize/resize.service";
 })
 export class AppComponent implements OnInit {
   title = 'Ricardo Pájaro Coatl';
-
-  constructor(
-    private resizeService: ResizeService) {
-  }
-
   ngOnInit(): void {
-    this.resizeService.onResize(window.innerWidth);
+
   }
 
-  onResize(event: any) {
-    this.resizeService.onResize(event.target.innerWidth);
-  }
+
+
 }
--- src/app/services/resize/resize.service.ts
+++ src/app/services/resize/resize.service.ts
@@ -1,25 +1,27 @@
-import {BehaviorSubject} from "rxjs";
-import {Injectable} from '@angular/core';
+import { BehaviorSubject, distinctUntilChanged, fromEvent, map, startWith } from "rxjs";
+import { Injectable } from '@angular/core';
+
+const MOBILE_SIZE = 600;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResizeService {
-  private _isMobileSize = new BehaviorSubject<boolean>(true);
+  private _isMobileSize = new BehaviorSubject<boolean>(window.innerWidth <= MOBILE_SIZE);
   isMobileSize = this._isMobileSize.asObservable();
 
+  private _widthSize = new BehaviorSubject<number>(window.innerWidth);
+  widthSize = this._widthSize.asObservable();
+
   constructor() {
+    fromEvent(window, 'resize').pipe(
+      map(() => window.innerWidth),
+      distinctUntilChanged(),
+      startWith(window.innerWidth)
+    ).subscribe(newSize => {
+      this._isMobileSize.next(newSize <= MOBILE_SIZE);
+      this._widthSize.next(newSize);
+    });
   }
 
-  private _widthSize = new BehaviorSubject<number>(600);
-
-  get widthSize() {
-    return this._widthSize.asObservable();
-  }
-
-
-  onResize(newSize: number) {
-    this._isMobileSize.next(newSize <= 600);
-    this._widthSize.next(newSize);
-  }
 }
