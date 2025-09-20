5812a54df8a09a834db3eaed9c9a0fd14eb2bfa6
Hcnc100 <ricardo.pajaro.bc2d@gmail.com>
2025-09-19 18:50:45 -0600

    refactor(app.routes): reemplazar componentes por carga diferida en las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,55 +1,5 @@
-
 import { Route } from '@angular/router';
-import { navigatorSections } from '../utils/Constants';
-import { ProfileComponent } from './profile/profile/profile.component';
-import { SkillsComponent } from './skills/skills/skills.component';
-import { ProjectsComponent } from './projects/projects/projects.component';
-import { CertificateComponent } from './certificate/certificate/certificate.component';
-import { ContactComponent } from './contact/contact/contact.component';
-import { TermsComponent } from './terms/terms/terms.component';
-
-// export const appRoutes: Route[] = [
-//     {
-//         path: '',
-//         redirectTo: `/${navigatorSections.homeId}`,
-//         pathMatch: 'full'
-//     },
-//     {
-//         path: navigatorSections.homeId,
-//         loadComponent: () =>
-//             import('./profile/profile/profile.component').then(m => m.ProfileComponent)
-//     },
-//     {
-//         path: navigatorSections.skillsId,
-//         loadComponent: () =>
-//             import('./skills/skills/skills.component').then(m => m.SkillsComponent)
-//     },
-//     {
-//         path: navigatorSections.projectId,
-//         loadComponent: () =>
-//             import('./projects/projects/projects.component').then(m => m.ProjectsComponent)
-//     },
-//     {
-//         path: navigatorSections.certificateId,
-//         loadComponent: () =>
-//             import('./certificate/certificate/certificate.component').then(m => m.CertificateComponent)
-//     },
-//     {
-//         path: navigatorSections.contactId,
-//         loadComponent: () =>
-//             import('./contact/contact/contact.component').then(m => m.ContactComponent)
-//     },
-//     {
-//         path: 'terms-and-conditions/:projectID',
-//         loadComponent: () =>
-//             import('./terms/terms/terms.component').then(m => m.TermsComponent)
-//     },
-//     {
-//         path: '**',
-//         redirectTo: `/${navigatorSections.homeId}`,
-//         pathMatch: 'full'
-//     }
-// ];
+import { navigatorSections } from 'src/utils/Constants';
 
 
 export const appRoutes: Route[] = [
@@ -60,27 +10,33 @@ export const appRoutes: Route[] = [
     },
     {
         path: navigatorSections.homeId,
-        component: ProfileComponent
+        loadComponent: () =>
+            import('./profile/profile/profile.component').then(m => m.ProfileComponent)
     },
     {
         path: navigatorSections.skillsId,
-        component: SkillsComponent
+        loadComponent: () =>
+            import('./skills/skills/skills.component').then(m => m.SkillsComponent)
     },
     {
         path: navigatorSections.projectId,
-        component: ProjectsComponent
+        loadComponent: () =>
+            import('./projects/projects/projects.component').then(m => m.ProjectsComponent)
     },
     {
         path: navigatorSections.certificateId,
-        component: CertificateComponent
+        loadComponent: () =>
+            import('./certificate/certificate/certificate.component').then(m => m.CertificateComponent)
     },
     {
         path: navigatorSections.contactId,
-        component: ContactComponent
+        loadComponent: () =>
+            import('./contact/contact/contact.component').then(m => m.ContactComponent)
     },
     {
         path: 'terms-and-conditions/:projectID',
-        component: TermsComponent
+        loadComponent: () =>
+            import('./terms/terms/terms.component').then(m => m.TermsComponent)
     },
     {
         path: '**',
