5bfeeb63e31b7dd37706a715dd1ccd599be21eb2
Hcnc100 <ricardo.pajaro.bc2d@gmail.com>
2025-09-24 20:50:14 -0600

    refactor: actualizar .gitignore y agregar archivo de configuración de entorno para Firebase

---
+++ src/environments/environment.template.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    siteKey: 'SITE_KEY_DEV_PLACEHOLDER',
+    firebase: {
+        apiKey: 'FIREBASE_API_KEY_DEV_PLACEHOLDER',
+        authDomain: 'FIREBASE_AUTH_DOMAIN_DEV_PLACEHOLDER',
+        projectId: 'FIREBASE_PROJECT_ID_DEV_PLACEHOLDER',
+        storageBucket: 'FIREBASE_STORAGE_BUCKET_DEV_PLACEHOLDER',
+        messagingSenderId: 'FIREBASE_MESSAGING_SENDER_ID_DEV_PLACEHOLDER',
+        appId: 'FIREBASE_APP_ID_DEV_PLACEHOLDER',
+        measurementId: 'FIREBASE_MEASUREMENT_ID'
+    }
+}
